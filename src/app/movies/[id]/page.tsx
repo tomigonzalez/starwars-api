f9a04216d52e467fc71045fea5727d5e22337be1
tomi <tomimateo2008@hormail.com>
2025-08-19 00:52:22 -0300

    asd

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -5,12 +5,16 @@ import { ChevronLeft, CircleUser, Clock } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
-interface Props {
-  params: { id: string };
+interface Params {
+  id: string;
 }
-const page = async ({ params }: Props) => {
+
+const Page = async ({ params }: { params: Promise<Params> }) => {
+  // Espera a que se resuelva params
+  const { id } = await params;
+
   // Traer película
-  const res = await fetch(`https://swapi.info/api/films/${params.id}`);
+  const res = await fetch(`https://swapi.info/api/films/${id}`);
   const film: Films = await res.json();
 
   // Traer planetas en paralelo
@@ -20,13 +24,13 @@ const page = async ({ params }: Props) => {
       return res.json();
     })
   );
-  // Función para formatear el número con puntos
+
+  // Función para formatear números
   const formatNumber = (num: number) => {
-    if (isNaN(num)) {
-      return num;
-    }
+    if (isNaN(num)) return num;
     return new Intl.NumberFormat("es-ES").format(num);
   };
+
   return (
     <section className="w-full h-full flex justify-center">
       <div className="sm:w-[1114px] sm:h-[755px] w-[344px] h-[1029px]">
@@ -41,14 +45,14 @@ const page = async ({ params }: Props) => {
         </Link>
 
         <div className="flex sm:flex-row flex-col justify-between mt-8 ">
-          <section className="sm:w-[392px] sm:h-[417px] w-full flex gap-4 flex-col border-b-2 border-primary  sm:pb-0 pb-6 sm:mb-0 mb-6">
+          <section className="sm:w-[392px] sm:h-[417px] w-full flex gap-4 flex-col border-b-2 border-primary sm:pb-0 pb-6 sm:mb-0 mb-6">
             <h5 className="text-sm leading-[20px] tracking-[-0.01em]">
               Opening
             </h5>
             {film.opening_crawl.split(/\r\n\r\n/).map((paragraph, i) => (
               <p
                 key={i}
-                className=" text-sm leading-[20px] tracking-[-0.01em] text-tertiary mb-3"
+                className="text-sm leading-[20px] tracking-[-0.01em] text-tertiary mb-3"
               >
                 {paragraph}
               </p>
@@ -56,15 +60,15 @@ const page = async ({ params }: Props) => {
 
             <div className="flex flex-row gap-2 ">
               <Clock className="bg-primary rounded-lg p-[4px]" />
-              <p className="text-sm  self-center">{film.release_date}</p>
+              <p className="text-sm self-center">{film.release_date}</p>
             </div>
             <div className="flex flex-row gap-2 ">
               <CircleUser className="bg-primary rounded-lg p-[4px]" />
-              <p className="text-sm  self-center">{film.director}</p>
+              <p className="text-sm self-center">{film.director}</p>
             </div>
             <div className="flex flex-row gap-2">
               <CircleUser className="bg-primary rounded-lg p-[4px]" />
-              <p className="text-sm  self-center">{film.producer}</p>
+              <p className="text-sm self-center">{film.producer}</p>
             </div>
           </section>
 
@@ -75,20 +79,20 @@ const page = async ({ params }: Props) => {
                 key={index}
                 className="bg-primary rounded-2xl p-6 sm:w-[607px] w-[344px] h-[168px] gap-4 flex flex-row justify-between"
               >
-                <div className="flex  justify-between flex-col h-[120px]">
+                <div className="flex justify-between flex-col h-[120px]">
                   <h4 className="text-lg">{planet.name}</h4>
                   <p className="text-sm text-tertiary">
                     {formatNumber(planet.population)} población
                   </p>
                   <p className="text-sm text-tertiary">
-                    {formatNumber(planet.diameter)} km de díametro
+                    {formatNumber(planet.diameter)} km de diámetro
                   </p>
                   <p className="text-sm text-tertiary">
                     Periodo de rotación: {planet.orbital_period} días
                   </p>
                 </div>
                 <button className="bg-[#6F00ED] hover:bg-[#6111bc] cursor-pointer backdrop-blur-xl rounded-xl w-[74px] h-8 text-sm">
-                  Ver mas
+                  Ver más
                 </button>
               </div>
             ))}
@@ -99,4 +103,4 @@ const page = async ({ params }: Props) => {
   );
 };
 
-export default page;
+export default Page;
